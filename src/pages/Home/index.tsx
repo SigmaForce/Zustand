6557e4a2a11a9a07ac91226004943cf78a814eb3
Leonardo Matos <leonardo.matos@ciarama.com.br>
2023-04-12 16:45:56 -0400

    :tada: Testando o Zustand - Gerenciador de estado

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import useUserStore, { UserType } from "../../stores/user";
+
+export const Home = () => {
+  const users = useUserStore((state) => state.users);
+  return (
+    <div>
+      {users?.map((user) => (
+        <p key={Math.random()}>
+          {user.name} | {user.email}
+        </p>
+      ))}
+    </div>
+  );
+};
